a170b456f0c52ca9d75e5ededf798d4180ada294
Sebastian Vilhelm Nielsen <seb@frankly.dk>
2018-04-30 13:48:16 +0200

    redux init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
+
+// Redux
+import { DevToolsExtension, NgRedux, NgReduxModule } from '@angular-redux/store';
+import { NgReduxRouter, NgReduxRouterModule } from '@angular-redux/router';
+import { createEpicMiddleware, combineEpics } from "redux-observable";
+import { createLogger } from 'redux-logger';
+import { IAppState, rootReducer } from './redux/store';
 
 
 @NgModule({
@@ -12,9 +20,33 @@ import { AppComponent } from './components/app/app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgReduxModule,
+    NgReduxRouterModule.forRoot(),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private devTool: DevToolsExtension,
+    private ngReduxRouter: NgReduxRouter
+  ) {
+
+    const enhancers = [devTool.enhancer()];
+    const middleware = [];
+
+    this.ngRedux.configureStore(
+      rootReducer,
+      {},
+      middleware,
+      enhancers
+    );
+
+    ngReduxRouter.initialize(/* args */);
+  }
+
+}
---
+++ src/app/redux/store.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+export class IAppState = { }
+
+export const rootState: IAppState = {};
+
+export const rootReducer = combineReducers<IAppState>({
+});
