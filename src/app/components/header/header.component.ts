d5cb5f828516d1a07c04011c80b536f6b0320090
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-03 10:47:13 +0200

    navigation styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  links: Object[] = [
+    {
+      name: 'Home',
+      path: ''
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
