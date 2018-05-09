cda2ed4c67d551c2e137257fa0c35504993f9882
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-09 13:16:20 +0200

    dinner mock

--- src/app/dinners/dinners.service.ts
+++ src/app/dinners/dinners.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DinnersState } from './dinners.store';
+import { Dinner } from '../entities/Dinner';
+import * as faker from 'faker';
 
 @Injectable()
 export class DinnersService {
@@ -9,6 +11,20 @@ export class DinnersService {
   static getInitalState(): DinnersState {
     return {
       dinners: []
+    };
+  }
+
+  static getMockDinner(): any {
+    return {
+      name: faker.random.words(),
+      streetAddress: faker.address.streetAddress(),
+      city: faker.address.city(),
+      host: {},
+      date: new Date(faker.date.future()),
+      attendeesMax: faker.random.number(),
+      menu: faker.random.words(),
+      description: faker.random.words(),
+      specifics: []
     };
   }
 
