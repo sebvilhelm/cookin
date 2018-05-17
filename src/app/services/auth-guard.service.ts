99d1b75412ade65ab4cf87a0c3ea14e406255cc4
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-17 11:26:25 +0200

    with redux subscription

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from '../store/store';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router) { }
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private router: Router,
+    private ngRedux: NgRedux<IAppState>
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.checkLogin();
   }
 
   checkLogin() {
-    // TODO: Check login
-    if (1 + 1 !== 2) {
+    this.ngRedux.select(state => state.users.currentUser).subscribe(user => {
+      this.isLoggedIn = user ? true : false;
+    });
+    if (!this.isLoggedIn) {
       this.router.navigate(['login']);
       return false;
     }
