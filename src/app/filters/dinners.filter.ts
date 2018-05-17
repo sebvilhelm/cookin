4299c55fdbf14eae659925dfa4f6a43549e36957
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-17 11:33:05 +0200

    filter init

---
+++ src/app/filters/dinners.filter.ts
@@ -0,0 +1,17 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { Dinner } from '../entities/Dinner';
+
+
+@Pipe({ name: 'filterDinners' })
+@Injectable()
+export class FilterDinners implements PipeTransform {
+  transform(items: Dinner[], args: string) {
+    if (args && items.length > 0) {
+      const filteredItems = items.filter(item =>
+        item.specifics.includes(args)
+      );
+      return filteredItems;
+    }
+    return items;
+  }
+}
